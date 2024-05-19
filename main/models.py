20b3e628738e8e3aab853764a72bbd9e538783a1
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-05-19 16:20:30 +0700

    Пересобрал фронт от zhozhyr

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class Service(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='services',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name='Наименование')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цeнa')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+    def __str__(self):
+        return self.name
+
