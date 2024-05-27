0a18021bfa38ba7583a75e164fc6e825270b5ac0
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-05-27 10:56:02 +0700

    upload last project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Service
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    list_editable = ['category']
