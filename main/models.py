e7560b615df1036e0fdcc3511c491d2d1304ebd6
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-05-27 13:35:19 +0700

    models&db

--- main/models.py
+++ main/models.py
@@ -8,11 +8,10 @@ class Category(models.Model):
         return self.name
 
 class Service(models.Model):
-    category = models.ForeignKey(Category,
-                                 related_name='services',
-                                 on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='services', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, verbose_name='Наименование')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цeнa')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    is_visible = models.BooleanField(default=True, verbose_name='Отображается на сайте')
 
     class Meta:
         ordering = ('name',)
@@ -21,4 +20,3 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
-
