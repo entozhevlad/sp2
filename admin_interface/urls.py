bdc8e1a04ca32e61c4c325c5d4f7b49ae03665c8
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-06-05 10:22:01 +0700

    save changes for password reset

--- admin_interface/urls.py
+++ admin_interface/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from admin_interface import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Другие URL-ы
     path('admin_sp/', views.admin_sp, name='admin_sp'),
     path('login/', views.login_view, name='login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
--- sp/settings.py
+++ sp/settings.py
@@ -139,3 +139,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/admin_sp/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp-mail.outlook.com'
+EMAIL_HOST_USER=env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
