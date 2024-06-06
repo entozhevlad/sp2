8079a0aa65de84ebb90f48d173a09c428d1f0813
Vladislav Zarubin <vladislav.zarubin113@gmail.com>
2024-06-06 10:00:16 +0700

    views, urls change for b2b. add form for b2b page

--- main/views.py
+++ main/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Service, Category
-
-
-# Create your views here.
+from django.core.mail import send_mail
+from django.conf import settings
+from contacts.forms import ContactForm
+import environ
+env = environ.Env()
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-class B2BPageView(TemplateView):
+def B2BPageView(request):
     template_name = 'b2b.html'
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+
+            full_message = f"Имя: {name}\nEmail: {email}\nТелефон: {phone}\n\nСообщение:\n{message}"
+
+            send_mail(
+                'Новое сообщение с сайта',
+                full_message,
+                settings.EMAIL_HOST_USER,
+                [env('STATIC_EMAIL')],
+                fail_silently=False,
+            )
+            return redirect('main:home')
+    else:
+        form = ContactForm()
+    return render(request, template_name, {'form': form})
+
 class USPageView(TemplateView):
     template_name = 'us.html'
 def service_view(request):
