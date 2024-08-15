4f7a30c9547092727e27be478bf332c8a26ce0f1
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-08-15 16:29:48 +0700

    шаблоны под новые требования

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from .views import *
 app_name = 'main'
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('b2b/', B2BPageView, name='b2b'),
-    path('us/',USPageView.as_view(), name='us'),
-    path('price/', service_view, name='price')
+    path('price/', service_view, name='price'),
+    path('catalog/', catalog_view, name='catalog'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,37 +5,20 @@ from django.core.mail import send_mail
 from django.conf import settings
 from contacts.forms import ContactForm
 import environ
+
 env = environ.Env()
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-def B2BPageView(request):
-    template_name = 'b2b.html'
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            phone = form.cleaned_data['phone']
-            message = form.cleaned_data['message']
 
-            full_message = f"Имя: {name}\nEmail: {email}\nТелефон: {phone}\n\nСообщение:\n{message}"
+def catalog_view(request):
+    services = Service.objects.filter(is_visible=True)
+    categories = Category.objects.all()
+    return render(request, 'catalog.html', {'categories': categories, 'services': services})
 
-            send_mail(
-                'Новое сообщение с сайта',
-                full_message,
-                settings.EMAIL_HOST_USER,
-                [env('STATIC_EMAIL')],
-                fail_silently=False,
-            )
-            return redirect('main:home')
-    else:
-        form = ContactForm()
-    return render(request, template_name, {'form': form})
 
-class USPageView(TemplateView):
-    template_name = 'us.html'
 def service_view(request):
     services = Service.objects.filter(is_visible=True)
     categories = Category.objects.all()
