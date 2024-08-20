9cb9f364098112b8d3aa21522e964bf87f86f12a
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-08-21 02:01:04 +0700

    готовая главная страница с примерами работ + готов адаптив под мобильные устройства главной страницы, добавлена фавиконка в декстоп-версию, залиты медиа-файлы с каталога

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,6 @@
-document.querySelector('.burger').addEventListener('click', function() {
-this.classList.toggle('active');
-document.querySelector('.navbar').classList.toggle('open');
-})
-
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector('.burger').addEventListener('click', function () {
+        this.classList.toggle('active');
+        document.querySelector('.navbar').classList.toggle('open');
+    })
 });
