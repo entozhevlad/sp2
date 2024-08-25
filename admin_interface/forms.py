074261e5d93059a26a498031e1d0db77653386aa
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-08-25 16:15:04 +0700

    Quicksave for admin_interface

--- admin_interface/forms.py
+++ admin_interface/forms.py
@@ -1,7 +1,7 @@
-# forms.py
 from django import forms
 from main.models import Category, Service
-
+from catalog.models import Color, ProductCategory, Product, ProductVariant
+from contacts.models import Adress
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
@@ -11,3 +11,34 @@ class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = ['category', 'name', 'price', 'is_visible']
+
+class ColorForm(forms.ModelForm):
+    class Meta:
+        model = Color
+        fields = ['name', 'image']
+
+class ProductCategoryForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ['name', 'slug']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'name', 'category', 'description', 'material', 'additional_details',
+            'height', 'size', 'density', 'diameter', 'application_method', 'volume'
+        ]
+
+class ProductVariantForm(forms.ModelForm):
+    class Meta:
+        model = ProductVariant
+        fields = [
+            'product', 'name', 'color', 'price', 'main_image',
+            'additional_image_1', 'additional_image_2', 'additional_image_3'
+        ]
+
+class AdressForm(forms.ModelForm):
+    class Meta:
+        model = Adress
+        fields = ['adress', 'graphic_1', 'graphic_2']
--- admin_interface/views.py
+++ admin_interface/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import CategoryForm, ServiceForm
+from .forms import CategoryForm, ServiceForm, ColorForm, ProductCategoryForm, ProductForm, ProductVariantForm, AdressForm
 from main.models import Category, Service
+from catalog.models import Color, ProductCategory, Product, ProductVariant
+from contacts.models import Adress
 
 @login_required
 def admin_sp(request):
@@ -86,3 +88,175 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def edit_color(request, color_id):
+    color = get_object_or_404(Color, id=color_id)
+    if request.method == 'POST':
+        form = ColorForm(request.POST, request.FILES, instance=color)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ColorForm(instance=color)
+    return render(request, 'edit_color.html', {'form': form})
+
+@login_required
+def delete_color(request, color_id):
+    color = get_object_or_404(Color, id=color_id)
+    if request.method == 'POST':
+        color.delete()
+        return redirect('admin_sp')
+    return render(request, 'confirm_delete.html', {'object': color, 'type': 'color'})
+
+# ProductCategory Views
+@login_required
+def add_product_category(request):
+    if request.method == 'POST':
+        form = ProductCategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductCategoryForm()
+    return render(request, 'add_product_category.html', {'form': form})
+
+@login_required
+def edit_product_category(request, category_id):
+    category = get_object_or_404(ProductCategory, id=category_id)
+    if request.method == 'POST':
+        form = ProductCategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductCategoryForm(instance=category)
+    return render(request, 'add_edit_product_category.html', {'form': form})
+
+@login_required
+def delete_product_category(request, category_id):
+    category = get_object_or_404(ProductCategory, id=category_id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('admin_sp')
+    return render(request, 'confirm_delete.html', {'object': category, 'type': 'product_category'})
+
+# Product Views
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductForm()
+    return render(request, 'add_edit_product.html', {'form': form})
+
+@login_required
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'add_edit_product.html', {'form': form})
+
+@login_required
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('admin_sp')
+    return render(request, 'confirm_delete.html', {'object': product, 'type': 'product'})
+
+# ProductVariant Views
+@login_required
+def add_product_variant(request):
+    if request.method == 'POST':
+        form = ProductVariantForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductVariantForm()
+    return render(request, 'add_edit_product_variant.html', {'form': form})
+
+@login_required
+def edit_product_variant(request, variant_id):
+    variant = get_object_or_404(ProductVariant, id=variant_id)
+    if request.method == 'POST':
+        form = ProductVariantForm(request.POST, request.FILES, instance=variant)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ProductVariantForm(instance=variant)
+    return render(request, 'add_edit_product_variant.html', {'form': form})
+
+@login_required
+def delete_product_variant(request, variant_id):
+    variant = get_object_or_404(ProductVariant, id=variant_id)
+    if request.method == 'POST':
+        variant.delete()
+        return redirect('admin_sp')
+    return render(request, 'confirm_delete.html', {'object': variant, 'type': 'product_variant'})
+
+# Adress Views
+@login_required
+def add_adress(request):
+    if request.method == 'POST':
+        form = AdressForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = AdressForm()
+    return render(request, 'add_edit_adress.html', {'form': form})
+
+@login_required
+def edit_adress(request, adress_id):
+    adress = get_object_or_404(Adress, id=adress_id)
+    if request.method == 'POST':
+        form = AdressForm(request.POST, instance=adress)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = AdressForm(instance=adress)
+    return render(request, 'add_edit_adress.html', {'form': form})
+
+@login_required
+def delete_adress(request, adress_id):
+    adress = get_object_or_404(Adress, id=adress_id)
+    if request.method == 'POST':
+        adress.delete()
+        return redirect('admin_sp')
+    return render(request, 'confirm_delete.html', {'object': adress, 'type': 'adress'})
+
+@login_required
+def add_color(request):
+    if request.method == 'POST':
+        form = ColorForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ColorForm()
+    return render(request, 'add_edit_color.html', {'form': form})
+
+@login_required
+def edit_color(request, color_id):
+    color = get_object_or_404(Color, id=color_id)
+    if request.method == 'POST':
+        form = ColorForm(request.POST, request.FILES, instance=color)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_sp')
+    else:
+        form = ColorForm(instance=color)
+    return render(request, 'add_edit_color.html', {'form': form})
