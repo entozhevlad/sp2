1c976c847a908f572d40352e4a088bebd8688bc8
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-08-29 01:48:20 +0700

    Рефакторинг

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,9 +1,11 @@
 document.querySelector('.category-toggle').addEventListener('click', function() {
     document.querySelector('.category-list').classList.toggle('show');
     document.querySelector('.overlay').classList.toggle('show');
+    document.body.classList.add('menu-open');
 });
 
 document.querySelector('.overlay').addEventListener('click', function() {
     document.querySelector('.category-list').classList.toggle('show');
     document.querySelector('.overlay').classList.toggle('show');
+    document.body.classList.remove('menu-open');
 });
