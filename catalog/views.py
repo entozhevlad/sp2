06b0e81d2083efd9dada3bfeeba229ec08c5fe88
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-08-29 00:20:43 +0700

    готовая карточка товара + адаптив под мобильные экраны; дополнена модель товара для карточки и каталога

--- catalog/views.py
+++ catalog/views.py
@@ -10,8 +10,6 @@ def catalog_view(request, category_slug=None):
         category = get_object_or_404(ProductCategory, slug=category_slug)
         variants = variants.filter(product__category=category)  # Фильтруем варианты по категории продукта
 
-    for variant in variants:
-        variant.price_display = f'{int(variant.price)}'
 
     context = {
         'categories': categories,
@@ -20,14 +18,28 @@ def catalog_view(request, category_slug=None):
     }
     return render(request, 'catalog.html', context)
 
+
+import random
+
+
 def product_detail(request, product_id):
     product = get_object_or_404(ProductVariant, id=product_id)
     other_variants = ProductVariant.objects.filter(
         product=product.product
     ).exclude(id=product.id)
 
+
+
+    # Выбираем случайные продукты для блока "ЕЩЕ"
+    random_products = list(
+        ProductVariant.objects.exclude(id=product.id).order_by('?')[:10])  # Например, 4 случайных продукта
+
+    for variant in random_products:
+        variant.price_display = int(variant.price)
+
     context = {
         'product': product,
         'other_variants': other_variants,  # Передаем другие варианты в контекст
+        'random_products': random_products,  # Передаем случайные продукты в контекст
     }
     return render(request, 'product_detail.html', context)
